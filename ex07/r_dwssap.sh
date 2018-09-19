cdaf4f7bc9a353728a2b78c4d245327e656a60aa
Sen Li <seli@e1z1r7p16.42.us.org>
2018-09-18 22:23:17 -0700

    using sed p from line to line instead of awk NR

--- ex07/r_dwssap.sh
+++ ex07/r_dwssap.sh
@@ -1,1 +1,1 @@
-cat /etc/passwd | grep -v '^#' | awk 'NR % 2 == 0' | cut -d ':' -f 1 | rev | sort -r | awk "(NR >= $FT_LINE1 && NR <= $FT_LINE2)" | tr '\n' ',' | sed 's/,$/./;s/,/, /g'
+cat /etc/passwd | grep -v '^#' | awk 'NR % 2 == 0' | cut -d ':' -f 1 | rev | sort -r | sed -n $FT_LINE1,$FT_LINE2\p | tr '\n' ',' | sed 's/,$/./;s/,/, /g'
