9fe724246668c11a0f870b8a3613ab1d6a60ecb1
Sen Li <seli@e1z1r7p16.42.us.org>
2018-09-19 11:38:40 -0700

    update permission

--- ex03/count_files.sh
+++ ex03/count_files.sh
@@ -1,8 +1,2 @@
+#!/bin/sh
 find . -type d -o -type f | wc -l | tr -d ' '
-# ls -la show all file/directories/link/xxxxx
-# -E pattern or
-# ^- match regular file
-# ^d match directories
-# wc -l count lines
-# awk get the first field and subtract 1 (the ..)
-
